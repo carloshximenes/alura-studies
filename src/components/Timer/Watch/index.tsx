f67a0d2882e26a4f1ced36fb20c068fc1b945198
Carlos Henrique <carloshximenes@gmail.com>
2022-08-29 21:43:15 -0300

    fix issues

--- src/components/Timer/Watch/index.tsx
+++ src/components/Timer/Watch/index.tsx
@@ -1,14 +1,15 @@
+import moment from "moment";
 import classes from "./watch.module.scss";
 
 const Watch: React.FC<{ time: number | undefined }> = (props) => {
     const { time = 0 } = props;
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
 
-    const timeAsArray = `${
-        String(minutes).padStart(2, '0') + String(seconds).padStart(2, '0')
-    }`;
-    const [md, mu, sd, su] = timeAsArray;   
+    const duration = moment.duration({ seconds: time });
+    const timeResult =
+        String(Math.floor(duration.asMinutes())).padStart(2, "0") +
+        String(duration.seconds()).padStart(2, "0");
+
+    const [md, mu, sd, su] = timeResult;
 
     return (
         <>
